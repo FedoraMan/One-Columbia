357963f2eb1a35804183fba38255428ec7baa875
Matthew Teague <teaguem27@yahoo.com>
2015-02-17 22:54:29 -0500

    Almost finished home page and started event page

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+
+
+
+
+	
+
+	$("#menuButton").click( function( e ){
+
+		$(".menuHide").slideToggle('fast');
+		e.preventDefault();
+	});
+
+	$("#menuButton2").click(function( e ){
+
+		$(".menuHide2").slideToggle('fast');
+
+		e.preventDefault();
+	});
+
+
+
+
+
+
+});
